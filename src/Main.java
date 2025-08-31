718e12ba78d5c42de115fa858ded693c1b99e93d
alejandruxxug <alejandro.urrego@eia.edu.co>
2025-08-30 21:58:25 -0500

    ok is this workinggg?

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello This is Sakura Enterprises, Please Identify yourself! 😊");
+        boolean loggedIn = false;
+
+        while (!loggedIn) {
+            System.out.println("Hello This is Sakura Enterprises, Please Identify yourself! 😊");
+            System.out.println("1. Log in" +
+                    "2. Create client account");
+            break;
+        }
+
 
     }
 }
